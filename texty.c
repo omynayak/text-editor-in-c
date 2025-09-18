8b16bbf0fe48ca6ba29aba45d472629a878b7713
omynayak <manelomnayak099@gmail.com>
2025-09-18 20:19:10 +0530

    Finished transition from canonical to raw mode

--- texty.c
+++ texty.c
@@ -1,19 +1,35 @@
-#include <unistd.h>
-#include <termios.h>
-#include <stdlib.h>
+/* ALL THE LIBRARIES INCLUDED*/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+/* DATA */
 
 struct termios orig_termios;
 
+
+/* TERMINAL */
+
+void die(const char* s){
+    perror(s);
+    exit(1);
+}
+
+
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -23,21 +39,30 @@ void enableRawMode()
     raw.c_cflag |= (CS8);
     raw.c_lflag = (raw.c_lflag) & ~(ECHO | ICANON | ISIG | IEXTEN);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+
+
+/* INITIALISATION */
 
 int main(void)
 {
     enableRawMode();
 
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
-        if(iscntrl(c)){
+    while(1){
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+
+        if(iscntrl(c)) {
             printf("%d\r\n",c);
         } else {
             printf("%d ('%c')\r\n",c,c);
         }
-    };
-    disableRawMode();
+
+        if(c == 'q') break;
+    }
     return 0;
 }
